c14be908de609a01588cc3ebb428d379c2aa401f
mostafaosama99 <dev.mostafaosama@gmail.com>
2022-07-20 17:28:00 +0200

    step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,96 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-import 'dart:math' as math show Random;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:developer' as devtools show log;
 
-
-const names = ['Mostafa', 'Medo', 'boody'];
-
-/// get random element extension
-extension RandomElement<T> on Iterable<T>{
-  T getRandomElement() => elementAt(math.Random().nextInt(length));
+extension Log on Object {
+  void log() => devtools.log(toString());
 }
 
-// creating Cubit
-// cubit and BLoC needs to know which type of State do i managing?
-// that's why we give it a type
+// when creating a BLoC we need to know what kind of EVENT does this BLoC accept
+// and what of a STATE does this BLoC produce
+// all BLoCs have multiple events, and produces multiple states
 
-// Cubit sets on top of Steams and StreamController, and they are very simple to use
-// BLoC sets on top of Cubit
-// BLoC could produce many states per event however redux produces only one
-// state per event
+// this to make the events from the same type
+@immutable
+abstract class LoadAction {
+  const LoadAction();
+}
 
-class NamesCubit extends Cubit<String?> {
-  /// Cubit and BLoC requires an initial state
-  NamesCubit() : super(null);
+enum PersonUrl { persons1, persons2 }
 
-  void pickRandomName() {
-    /// emit used to change the state
-    emit(names.getRandomElement());
+extension UrlString on PersonUrl {
+  String get urlString {
+    switch (this) {
+      case PersonUrl.persons1:
+        return 'http://127.0.0.1:5500/api/persons1.json';
+      case PersonUrl.persons2:
+        return 'http://127.0.0.1:5500/api/persons2.json';
+    }
   }
 }
 
+extension Subscript<T> on Iterable<T> {
+  T? operator [](int index) =>
+      (index >= 0 && index < length) ? elementAt(index) : null;
+}
+
+// getting persons
+Future<Iterable<Person>> getPersons(String url) => HttpClient()
+    .getUrl(Uri.parse(url))
+    .then((request) => request.close())
+    .then((response) => response.transform(utf8.decoder).join())
+    .then((str) => json.decode(str) as List)
+    .then((list) => list.map((e) => Person.fromJson(e)));
+
+@immutable
+class FetchResult {
+  final bool isRetrievedFromCash;
+  final Iterable<Person> persons;
+
+  const FetchResult({required this.isRetrievedFromCash, required this.persons});
+
+  @override
+  String toString() => 'FetchResults:\n\tis Retrieved from cash: '
+      '$isRetrievedFromCash\n\tPersons: $persons';
+}
+
+@immutable
+class LoadPersonsAction implements LoadAction {
+  final PersonUrl url;
+
+  const LoadPersonsAction({required this.url});
+}
+
+class Person {
+  late final String name;
+  late final int age;
+
+  Person.fromJson(Map<String, dynamic> json) {
+    age = json['age'];
+    name = json['name'];
+  }
+}
+
+// here it comes
+class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<String, Iterable<Person>> _cash = {};
+
+  PersonsBloc() : super(null) {
+    on<LoadPersonsAction>((event, emit) async {
+      final url = event.url;
+      if (_cash[url] != null) {
+        emit(FetchResult(isRetrievedFromCash: true, persons: _cash[url]!));
+      } else {
+        final result = await getPersons(url.urlString);
+        _cash[url.urlString] = result;
+        emit(FetchResult(isRetrievedFromCash: false, persons: result));
+      }
+    });
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -37,7 +99,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,7 +106,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home:
+          BlocProvider(create: (_) => PersonsBloc(), child: const MyHomePage()),
     );
   }
 }
@@ -58,53 +120,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  late final NamesCubit cubit;
-
-  @override
-  void initState() {
-    super.initState();
-    cubit = NamesCubit();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    cubit.close();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter BLoC'),
       ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () => context
+                      .read<PersonsBloc>()
+                      .add(const LoadPersonsAction(url: PersonUrl.persons1)),
+                  child: const Text('load json #1')),
+              TextButton(
+                  onPressed: () => context
+                      .read<PersonsBloc>()
+                      .add(const LoadPersonsAction(url: PersonUrl.persons1)),
+                  child: const Text('load json #2')),
+            ],
+          ),
+          BlocBuilder<PersonsBloc, FetchResult?>(
+              buildWhen: (previousState, currentState) {
+            return previousState?.persons != currentState?.persons;
+          }, builder: (_, state) {
+            state?.log();
 
-
-      // as cubit state changes we need to update our home page as well
-      // so we use stream builder
-      body: StreamBuilder<String?>(
-          stream: cubit.stream,
-          builder: (context, snapshot) {
-            final button = TextButton(onPressed: () => cubit.pickRandomName(),
-                child: const Text('Pick a random name'));
-
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return button;
-              case ConnectionState.waiting:
-                return button;
-              case ConnectionState.active:
-                return Column(
-                  children: [
-                    Text(snapshot.data ?? 'empty'),
-                    button
-                  ],
-                );
-              case ConnectionState.done:
-                return const SizedBox();
+            if (state == null) {
+              return const SizedBox(
+                  height: 100, child: Center(child: Text('Nothing here')));
             }
-          }
+            return Expanded(
+                child: ListView.builder(
+                    itemBuilder: (_, index) => ListTile(
+                          title: Text(state.persons[index]!.name),
+                          subtitle: Text(state.persons[index]!.age.toString()),
+                        )));
+          })
+        ],
       ),
     );
   }
